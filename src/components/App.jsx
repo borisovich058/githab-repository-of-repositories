b20757d6c777878f85fa7b53e971ecc892ba440e
Ilya Dergunov <i.dergunov@itq-group.com>
2023-04-10 18:13:39 +0300

    001 - Настроен webpack + babel подключен реакт + redux

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.less';
+
+const app = () => {
+    return (
+        <div className='app'>
+            React work!!!
+        </div>
+    );
+};
+
+export default app;
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: ["@babel/polyfill", "./src/index.jsx"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[hash].js",
   },
   devServer: {
     port: 3000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "./src/index.html" }),
@@ -26,6 +29,26 @@ module.exports = {
         test: /\.(jpeg|jpg|png|svg)/,
         use: ["file-loader"],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.m?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"]
+          }
+        }
+      }
     ],
   },
 };
