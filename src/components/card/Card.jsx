66ab584ad677af295a398598ebd4d59aa016356c
Ilya Dergunov <i.dergunov@itq-group.com>
2023-05-31 23:29:59 +0300

    007 - fix button back in card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { redirect, useParams } from "react-router-dom";
 import { getContributors, getCurrentRepos } from "../actions/repos";
+import { useNavigate } from "react-router-dom";
 import "./card.less";
 
 const Card = (props) => {
   const { username, reponame } = useParams();
   const [repo, setRepo] = useState({ owner: {} });
   const [contributors, setContributors] = useState([]);
+
+  let navigate = useNavigate();
 
   useEffect(() => {
     getCurrentRepos(username, reponame, setRepo);
@@ -15,7 +18,7 @@ const Card = (props) => {
 
   return (
     <div>
-      <button onClick={() => redirect("/")}>back</button>
+      <button onClick={() => navigate("/")}>back</button>
       <div className="card">
         <img src={repo.owner.avatar_url} alt="avatar" />
         <div className="name">{repo.name}</div>
